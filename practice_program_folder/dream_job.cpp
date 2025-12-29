02d502b0cd634002f5765e06bceca5c974d5bc85
Keerthika Balakrishnan <keerthikabala2007@gmail.com>
2025-12-29 22:16:38 +0530

    Dream job code

---
+++ practice_program_folder/dream_job.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include <string>
+using namespace std;
+int main() {
+    string name;
+    string dreamjob;
+    cout<< "Enter your name: ";
+    cin>> name;
+    cout<< "Enter your dream job: ";
+    cin >> dreamjob;
+    cout<< "hello " << name << " your dream job is " << dreamjob << endl;
+    cout << "GOOD LUCK FOR YOUR DREAM JOB " << endl;
+    return 0;
+
+}
