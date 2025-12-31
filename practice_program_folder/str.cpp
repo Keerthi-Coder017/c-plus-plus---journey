adffd7658f70ee6fe8b94f725e6ea7d7aaf1c365
Keerthika Balakrishnan <keerthikabala2007@gmail.com>
2025-12-31 21:18:00 +0530

    Adding strings

---
+++ practice_program_folder/str.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main() {
+    string s1 = "Hello, ";
+    string s2 = "World!";
+    string s3 = " I am Keerthika.";
+    cout << s1+s2+s3 << endl;
+    return 0;
+}
